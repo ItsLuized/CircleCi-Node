a4083de627c171c8e4fa5687ccbc4b1286990c77
Luis Martinez <luisemartinezcruz@gmail.com>
2020-10-28 13:58:55 -0500

    Change way of starting server

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const port = 3000
 
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
